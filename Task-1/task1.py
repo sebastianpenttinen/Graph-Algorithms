77624873c2204fe3a48cb6b720a37f865839dfc1
sebastianpenttinen <sepentti@abo.fi>
2019-09-17 22:58:26 +0300

    added comments and description of algorithm

--- Task-1/task1.py
+++ Task-1/task1.py
@@ -3,13 +3,19 @@ import pandas as pd
 from tkinter.filedialog import askopenfilename
 
 def Matrix(edges):
-    keyNodes = sorted(edges.keys()) # Sort the nodes rows?
-    valueNodes = dict(zip(keyNodes, range(len(keyNodes)))) #columns? # make them the same shape
-    matrix = np.zeros(shape=(len(edges),len(edges)))
-    for i in range(len(keyNodes)):
-        for j in range(i, len(edges)):
-            if edges[keyNodes[i]] in valueNodes: # Check if there is an edge
-                j = valueNodes[edges[keyNodes[i]]]
+    # varaibles named according to what the value in the dictionaries are
+    nodes = sorted(edges.keys()) # Sort the nodes according to index then node as value
+    index = dict(zip(nodes, range(len(nodes)))) # here the node is the value and the index is the key 
+    matrix = np.zeros(shape=(len(edges),len(edges))) # make the n x n matrix
+    for i in range(len(nodes)): # go row by row
+        for j in range(i, len(edges)): # column by column
+             # Check if there is an edge by looking it up in the dictionaries
+             # First it checks the node value to find the node in the edges and then looks up that nodes index
+             # Example: looks up node at index 0 values is: 'n0'. Then uses 'n0' as the key to find if 'n0' has an edge to an node.
+             # If there is an edge it looks up the index of that edge from the index dict by using the found node as the key.
+             # In this case 'n0' has an edge to 'n33' and 'n33' has an index of 16. As a result a 1 is placed on row 0 column 16.
+            if edges[nodes[i]] in index:
+                j = index[edges[nodes[i]]] # put one in column if there is an edge
                 matrix[i,j] = 1
     return matrix
 
