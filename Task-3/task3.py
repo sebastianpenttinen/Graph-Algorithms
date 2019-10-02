09734875742c48fae8902e1d841717db62d84fd4
sebastianpenttinen <sepentti@abo.fi>
2019-10-03 00:52:13 +0300

    adds output of mst

--- Task-3/task3.py
+++ Task-3/task3.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import pandas as pd
 
+
 class Graph:
     def __init__(self):
         self.nodes = []
@@ -13,13 +14,14 @@ class Graph:
             self.edges[endNode].append(startNode)
         self.weights[(startNode, endNode)] = weight
         self.weights[(endNode, startNode)] = weight
-    
+
     def addNode(self, node):
         if node not in self.nodes:
             self.nodes.append(node)
 
+
 def getFileName():
-    #filename = askopenfilename()
+    # filename = askopenfilename()
     filename = "benchmark3.csv"
     return filename
 
@@ -38,25 +40,43 @@ def makeGraph(graph, data, graphType):
         else:
             graph.addEdge(row["Vertex1"], row["Vertex2"], row["weight"], False)
 
+
+def lookupId(data, edges):
+    ids = []
+    for i in edges[0]:
+        edgeID = data.loc[(data["Vertex1"] == i[0]) & (data["Vertex2"] == i[1])]
+        tmp = edgeID["id"].tolist()
+        if len(tmp) != 0:
+            ids.append(tmp[0])
+    return ids
+
+
+def writeTofile(tree):
+    f = open("MinimumSpanningTree.txt", "w")
+    for i in tree:
+        f.write(i + ",")
+    f.close()
+
+
 def prim(graph):
-    
+
     minimumSpanningTree = set()
     visitedNodes = set()
-
-    # select an arbitrary vertex to begin with
+    totalWeight = 0
+    # select a vertex to begin with
     if len(graph.nodes) != 0:
         visitedNodes.add(graph.nodes[0])
     while len(visitedNodes) != len(graph.nodes):
         crossRoad = set()
-        # for each element i in VisitedNodes, add the edge (x, k) to crossRoad if
-        # k is not in X
         for i in visitedNodes:
             for j in graph.nodes:
-                if j not in visitedNodes and graph.weights.get((i,j)) != 0:
-                    crossRoad.add((i,j))
+                if j not in visitedNodes and graph.weights.get((i, j)) != 0:
+                    crossRoad.add((i, j))
+
         # find the edge with the smallest weight in crossRoad
+        # This can be improved
         weight = 1000000000
-        minEdge = ("","")
+        minEdge = ("", "")
         for index in crossRoad:
             tmp = graph.weights.get(index)
             if tmp is not None:
@@ -65,17 +85,21 @@ def prim(graph):
                     minEdge = index
 
         minimumSpanningTree.add(minEdge)
-        
+
         visitedNodes.add(minEdge[1])
-    return minimumSpanningTree
+        totalWeight += weight
+
+    return minimumSpanningTree, totalWeight
+
 
 def main():
     filename = getFileName()
     data = readFile(filename)
     g = Graph()
 
-    makeGraph(g, data, 'u')
-    minimumSpanningTree = prim(g)
-    print(minimumSpanningTree)
+    makeGraph(g, data, "u")
+    writeTofile(lookupId(data, prim(g)))
+
+
 if __name__ == "__main__":
     main()
