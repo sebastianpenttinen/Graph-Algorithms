74a375a0cdab0dc2ac7cf6cea2c26b1ac9b3fe4a
sebastianpenttinen <sepentti@abo.fi>
2019-10-10 22:39:14 +0300

    added solution

--- Task-4/task4.py
+++ Task-4/task4.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from collections import defaultdict
 from tkinter.filedialog import askopenfilename
+import csv
+
 
 class Graph:
     def __init__(self):
@@ -17,10 +19,12 @@ class Graph:
         if node not in self.nodes:
             self.nodes.append(node)
 
+
 def getFileName():
-    #filename = askopenfilename()
+    # filename = askopenfilename()
     filename = "benchmark4.csv"
     return filename
+
 
 def readFile(filename):
     data = pd.read_csv(filename, sep="\t")
@@ -32,11 +36,20 @@ def makeGraph(graph, data):
         graph.addNode(row["Vertex1"])
         graph.addNode(row["Vertex2"])
         graph.addEdge(row["Vertex1"], row["Vertex2"], True)
-        
+
+
+def writeToCSV(coloredGraph):
+    with open("VertexColors.csv", mode="w") as csvFile:
+        fieldnames = ["Vertex-id", "Vertex-color"]
+        writer = csv.DictWriter(csvFile, fieldnames=fieldnames, delimiter="\t")
+        writer.writeheader()
+        for key, value in coloredGraph.items():
+            writer.writerow({"Vertex-id": key, "Vertex-color": value})
+
 
 def nodeColoring(graph):
     sortedNodes = sorted(graph.edges.items(), key=lambda i: len(i[1]), reverse=True)
-    
+
     nodeColors = {}
 
     for node in sortedNodes:
@@ -53,13 +66,18 @@ def nodeColoring(graph):
     return nodeColors
 
 
+def edgeColoring(graph):
+    # Did not manage to solve this
+    pass
+
+
 def main():
     filename = getFileName()
     data = readFile(filename)
     g = Graph()
     makeGraph(g, data)
-    color = nodeColoring(g)
-    print (color)
+    writeToCSV(nodeColoring(g))
+
 
 if __name__ == "__main__":
     main()
